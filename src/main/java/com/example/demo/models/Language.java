561b49f0c3e753b01e4e49c1903135e9219b87d8
Notnik_KG <tasnim0800@gmail.com>
2022-07-12 13:59:51 +0600

    added book class

---
+++ src/main/java/com/example/demo/models/Language.java
@@ -0,0 +1,13 @@
+package com.example.demo.models;
+
+public enum Language {
+
+    KYRGYZ_LANGUAGE("Кыргызский язык"),
+
+    RUSSIAN_LANGUAGE("Русский язык"),
+
+    ENGLISH("Английский язык");
+
+    Language(String language) {
+    }
+}
